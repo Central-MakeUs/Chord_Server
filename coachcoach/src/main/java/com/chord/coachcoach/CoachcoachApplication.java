8ea2a2972a6dfcdea4f3fe5de1d7fa9f65fa1b9d
subin930 <bread0930@g.skku.edu>
2025-12-29 21:07:51 +0900

    [Docs] 프로젝트 생성
    프로젝트 생성 및 .gitignore 파일 생성

---
+++ coachcoach/src/main/java/com/chord/coachcoach/CoachcoachApplication.java
@@ -0,0 +1,13 @@
+package com.chord.coachcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachcoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachcoachApplication.class, args);
+    }
+
+}
---
+++ coachcoach/src/test/java/com/chord/coachcoach/CoachcoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chord.coachcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachcoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
