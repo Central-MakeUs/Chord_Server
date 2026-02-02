1dffe2badafae42b5555eff1b57060f6b2e9b032
subin930 <bread0930@g.skku.edu>
2026-02-02 18:04:54 +0900

    [Fix] request null 처리 수정

--- coachcoach/catalog/src/main/java/com/coachcoach/catalog/dto/request/CheckDupRequest.java
+++ coachcoach/catalog/src/main/java/com/coachcoach/catalog/dto/request/CheckDupRequest.java
@@ -2,11 +2,18 @@ package com.coachcoach.catalog.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public record CheckDupRequest (
         @NotBlank(message = "메뉴명 입력은 필수입니다.")
         String menuName,
 
         List<String> ingredientNames
-) {}
+) {
+        public CheckDupRequest{
+                ingredientNames = Objects.requireNonNullElse(ingredientNames, Collections.emptyList());
+        }
+}
--- coachcoach/catalog/src/main/java/com/coachcoach/catalog/dto/request/MenuCreateRequest.java
+++ coachcoach/catalog/src/main/java/com/coachcoach/catalog/dto/request/MenuCreateRequest.java
@@ -5,20 +5,22 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public record MenuCreateRequest (
     /* 메뉴 정보 */
     @NotBlank(message = "메뉴 카테고리 입력은 필수입니다.")
     String menuCategoryCode,
-    
+
     @NotBlank(message = "메뉴명 입력은 필수입니다.")
     String menuName,
-    
+
     @NotNull(message = "판매가 입력은 필수입니다.")
     @Positive(message = "판매가는 0보다 커야 합니다.")
     BigDecimal sellingPrice,
-    
+
     @NotNull(message = "제조시간 입력은 필수입니다.")
     @Positive(message = "제조시간은 0보다 커야 합니다.")
     Integer workTime,
@@ -26,4 +28,9 @@ public record MenuCreateRequest (
     /* 레시피 정보 */
     List<RecipeCreateRequest> recipes,          // 기존 재료로 레시피 등록
     List<NewRecipeCreateRequest> newRecipes    // 새로운 재료로 레시피 등록
-) {}
+) {
+    public MenuCreateRequest {
+        recipes = Objects.requireNonNullElse(recipes, Collections.emptyList());
+        newRecipes = Objects.requireNonNullElse(newRecipes, Collections.emptyList());
+    }
+}
