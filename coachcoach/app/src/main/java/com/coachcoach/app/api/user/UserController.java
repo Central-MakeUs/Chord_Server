60b0aac8d71a2935241cb5bf0be15f659887fbc5
subin930 <bread0930@g.skku.edu>
2026-02-09 20:16:14 +0900

    [Feat] 매장 정보 수정

--- coachcoach/app/src/main/java/com/coachcoach/app/api/user/UserController.java
+++ coachcoach/app/src/main/java/com/coachcoach/app/api/user/UserController.java
@@ -1,13 +1,18 @@
 package com.coachcoach.app.api.user;
 
+import com.coachcoach.common.exception.BusinessException;
 import com.coachcoach.common.security.userdetails.CustomUserDetails;
+import com.coachcoach.user.domain.Store;
 import com.coachcoach.user.dto.request.OnboardingRequest;
+import com.coachcoach.user.dto.request.UpdateStoreRequest;
+import com.coachcoach.user.exception.UserErrorCode;
 import com.coachcoach.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "유저", description = "유저 관리 API")
@@ -39,5 +44,17 @@ public class UserController {
             @AuthenticationPrincipal CustomUserDetails details
     ) {
        userService.deleteUser(Long.valueOf(details.getUserId()));
+    }
+
+    /**
+     * 매장 정보 수정
+     */
+    @Operation(summary = "매장 정보 수정")
+    @PatchMapping("/stores")
+    public void updateStore(
+            @AuthenticationPrincipal CustomUserDetails details,
+            @RequestBody  UpdateStoreRequest request
+    ) {
+        userService.updateStore(Long.valueOf(details.getUserId()), request);
     }
 }
