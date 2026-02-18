7c9e89262050d21b78933e1b7e587c9ae6032ef2
subin930 <bread0930@g.skku.edu>
2026-02-18 15:38:20 +0900

    [Feat] 스케줄러 테스트

--- coachcoach/app/src/main/java/com/coachcoach/scheduler/Scheduler.java
+++ coachcoach/app/src/main/java/com/coachcoach/scheduler/Scheduler.java
@@ -1,15 +1,38 @@
 package com.coachcoach.scheduler;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class Scheduler {
 
-    @Scheduled(cron = "0 0 22 * * Sun")
-    public void scheduler() {
+    private final RestTemplate restTemplate;
 
+    @Value("${PRIVATE_SERVER_URL}")
+    private String privateServerUrl;
+
+
+    @PostConstruct
+    public void init() {
+        generateInsightScheduler();
+    }
+
+    @Scheduled(cron = "0 0 22 * * Sun")
+    public void generateInsightScheduler() {
+        try {
+            restTemplate.postForObject(privateServerUrl + "/insights", null, null);
+            log.info("{} 전략 생성 성공", LocalDateTime.now());
+        } catch (Exception e) {
+            log.error("{} 전략 생성 실패: {}", LocalDateTime.now(), e.getMessage());
+        }
     }
 }
